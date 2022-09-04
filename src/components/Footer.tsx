ff24c8bcd0b9c97f46203ec4b230f8b9fe19533d
Berk Çebi <berk@zeplin.io>
2022-09-04 17:56:29 +0300

    Add link for feedback

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,16 @@ function Header() {
     return (
         <footer className="Footer">
             Press P to start/stop playback, 1 to 7 to mute/unmute tracks.
+            <br />
+            Ping me at{" "}
+            <a
+                href="https://twitter.com/berkcebi"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                @berkcebi
+            </a>{" "}
+            for any feedback.
         </footer>
     );
 }
