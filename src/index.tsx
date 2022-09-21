4a521e93bf0c83fb6ee92b14e9afe989565d303c
Berk Çebi <berk@zeplin.io>
2022-09-21 12:32:42 -0700

    Add Amplitude

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as amplitude from "@amplitude/analytics-browser";
 import "./index.css";
 import App from "./components/App";
+
+if (import.meta.env.PROD) {
+    amplitude.init(import.meta.env.VITE_AMPLITUDE_API_KEY);
+    amplitude.track("Visit App");
+}
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
