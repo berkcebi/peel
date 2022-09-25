e773f8ce95941bca3567ab75e6584c3c76a1eb72
Berk Çebi <berk@zeplin.io>
2022-09-24 22:12:41 -0700

    Add Netlify configuration

--- api/functions/hello.ts
+++ api/functions/hello.ts
@@ -3,6 +3,9 @@ import { Handler } from "@netlify/functions";
 const handler: Handler = async () => {
     return {
         statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify({ message: "Hello, Peel!" }),
     };
 };
